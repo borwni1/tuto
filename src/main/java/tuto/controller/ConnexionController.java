4b9db05316bb72a8620990247d1894f74c2358fe
= <=>
2026-03-04 14:46:27 +0100

    Creation sous repertoire

--- src/main/java/tuto/controller/ConnexionController.java
+++ src/main/java/tuto/controller/ConnexionController.java
@@ -8,12 +8,12 @@ public class ConnexionController {
 
 	@GetMapping( "/" )
 	public String page1() {
-		return "home.html";
+		return "connexion/home.html";
 	}
 
 	@GetMapping( "/hello" )
 	public String page2() {
-		return "bonjour.html";
+		return "connexion/bonjour.html";
 	}
 
 }
