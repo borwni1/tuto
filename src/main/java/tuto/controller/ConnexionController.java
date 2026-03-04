ce0e7420fbdda743bab5ab48791db66ee8c8d191
= <=>
2026-03-04 16:02:49 +0100

    ajout de prg et garder les entrees eronnees de l'utilisateur pour qu'il puisse les changer

--- src/main/java/tuto/controller/ConnexionController.java
+++ src/main/java/tuto/controller/ConnexionController.java
@@ -3,35 +3,45 @@ package tuto.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @SessionAttributes( "connected" )
 public class ConnexionController {
 
 	@GetMapping( "/" )
-	public String page1() {
+	public String home() {
 		return "connexion/home.html";
 	}
 
-	@GetMapping( "/connect" )
-	public String page2( String username, String password, Model model ) {
+	@PostMapping( "/connect" )
+	public String connect( String username, String password, Model model ) {
 		var sb = new StringBuilder( username );
 		if ( sb.reverse().toString().equals( password ) ) {
 			model.addAttribute( "connected", username );
-			return "connexion/bonjour.html";
+			return "redirect:/bonjour";
 		} else {
 			model.addAttribute( "alert", "Mot de passe incorrect" );
+			model.addAttribute( "username", username );
+			model.addAttribute( "password", password );
 			return "connexion/home.html";
 		}
 		// return "connexion/bonjour.html";
 	}
 
-	@GetMapping( "/disconnect" )
-	public String disconnect( SessionStatus status ) {
+	@PostMapping( "/disconnect" )
+	public String disconnect( SessionStatus status, RedirectAttributes ra ) {
 		status.setComplete();
+		ra.addFlashAttribute( "alert", "Déconnexion effectuée avec succès" );
 		return "redirect:/";
+	}
+
+	@GetMapping( "/bonjour" )
+	public String bonjour() {
+		return "connexion/bonjour.html";
 	}
 
 }
